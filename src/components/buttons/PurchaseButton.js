73150a727695171824d3def8260846e166a38a06
soundlotstudios <eaodesigns@gmail.com>
2022-01-18 15:39:02 -0800

    Sections 6-8
    Design+Code sections 6-8

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -8,8 +8,14 @@ export default function PurchaseButton(props) {
   return (
     <Link to="/page-2">
       <Wrapper>
-        <Title>{title || "Get Pro Access"}</Title>
-        <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        <IconWrapper>
+          <Icon src="/images/icons/credit.svg" className="icon" />
+          <Ring src="/images/icons/icon-ring.svg" />
+        </IconWrapper>
+        <TextWrapper>
+          <Title>{title || "Get Pro Access"}</Title>
+          <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        </TextWrapper>
       </Wrapper>
     </Link>
   )
@@ -24,6 +30,26 @@ const Wrapper = styled.div`
     0px 20px 40px rgba(23, 0, 102, 0.2),
     inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
+  display: grid;
+  grid-template-columns: 53px auto;
+  justify-content: start;
+  align-items: center;
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 30px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+    transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
+  }
 `
 
 const Title = styled(Caption2)`
@@ -33,4 +59,38 @@ const Title = styled(Caption2)`
 const Subtitle = styled(SmallText)`
   color: black;
   opacity: 0.7;
+`
+
+const IconWrapper = styled.div`
+  width: 45px;
+  height: 45px;
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+  border-radius: 50%;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(100%) saturate(120%);
+  }
+`
+
+const Icon = styled.img`
+  width: 29px;
+  height: 29px;
+`
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 4px;
+`
+
+const Ring = styled.img`
+  position: absolute;
+  top: -15px;
+  left: -16px;
+
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
