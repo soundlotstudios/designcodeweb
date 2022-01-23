129b6f92df213d806001166188a709b6e7e57090
soundlotstudios <eaodesigns@gmail.com>
2022-01-23 12:55:17 -0800

    Section 12
    Section 12 (Design+Code)

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,33 +1,58 @@
 import { Link } from "gatsby"
 import React from "react"
-
-const menuData = [
-  {
-    title: "Courses",
-    icon: "/images/icons/courses.svg",
-    link: "/courses",
-  },
-  {
-    title: "Tutorials",
-    icon: "/images/icons/tutorials.svg",
-    link: "/tutorials",
-  },
-  {
-    title: "Pricing",
-    icon: "/images/icons/pricing.svg",
-    link: "/pricing",
-  },
-]
+import styled from "styled-components"
+import { menuData } from "../../data/menuData"
 
 export default function Header() {
   return (
-    <>
-      {menuData.map((item, index) => (
-        <Link to={item.link} key={index}>
-          <img src={item.icon} alt="{item.title}" />
-          {item.title}
-        </Link>
-      ))}
-    </>
+    <Wrapper>
+      <Link to="/">
+        <img src="/images/logos/logo.svg" />
+      </Link>
+      <MenuWrapper>
+        {menuData.map((item, index) => (
+          <Link to={item.link} key={index}>
+            <MenuItem>
+              <img src={item.icon} alt={item.title} />
+              {item.title}
+            </MenuItem>
+          </Link>
+        ))}
+      </MenuWrapper>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px auto;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+`
+
+const MenuWrapper = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: repeat(5, auto);
+`
+
+const MenuItem = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  display: grid;
+  grid-template-columns: 24px auto;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  transition: 0.5s ease-out;
+  border-radius: 10px;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  }
+`
