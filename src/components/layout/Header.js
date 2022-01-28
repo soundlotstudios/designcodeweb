75d090e6d920d418bd5305ee9ba088bd39027bc5
soundlotstudios <eaodesigns@gmail.com>
2022-01-27 23:14:04 -0800

    Section 15
    Section 15 (design+code)

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,22 +1,31 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { menuData } from "../../data/menuData"
 import MenuButton from "../buttons/MenuButton"
 import MenuTooltip from "../tooltips/MenuTooltip"
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <Wrapper>
       <Link to="/">
         <img src="/images/logos/logo.svg" />
       </Link>
       <MenuWrapper count={menuData.length}>
-        {menuData.map((item, index) => (
-          <MenuButton item={item} key={index} />
-        ))}
+        {menuData.map((item, index) =>
+          item.link === "/account" ? (
+            <MenuButton
+              item={item}
+              key={index}
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          ) : (
+            <MenuButton item={item} key={index} />
+          )
+        )}
       </MenuWrapper>
-      <MenuTooltip />
+      <MenuTooltip isOpen={isOpen} />
     </Wrapper>
   )
 }
--- src/components/tooltips/MenuTooltip.js
+++ src/components/tooltips/MenuTooltip.js
@@ -3,11 +3,10 @@ import styled from "styled-components"
 import { tooltipData } from "../../data/menuData"
 import MenuButton from "../buttons/MenuButton"
 
-export default function MenuTooltip() {
-  const [isOpen, setIsOpen] = useState(false)
-
+export default function MenuTooltip(props) {
+  const { isOpen } = props
   return (
-    <Wrapper isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
+    <Wrapper isOpen={isOpen}>
       {tooltipData.map((item, index) => (
         <MenuButton item={item} key={index} />
       ))}
@@ -27,9 +26,16 @@ const Wrapper = styled.div`
   position: absolute;
   top: 60px;
   right: 30px;
-  opacity: ${props => (props.isOpen ? 1 : 0.5)};
+  opacity: ${props => (props.isOpen ? 1 : 0)};
   z-index: 1;
   display: grid;
   gap: 10px;
   grid-template-columns: 150px;
+  transition: 0.3s ease-in-out;
+  /* display: ${props => (props.isOpen ? "block" : "none")}; */
+  visibility: ${props => (props.isOpen ? "visible" : "hidden")};
+  transform: ${props =>
+    props.isOpen
+      ? "skewY(0) rotate(0) translateY(0)"
+      : "skewY(-5deg) rotate(5deg) translateY(-30px)"};
 `
